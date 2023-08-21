fa7739fccd07b00cb2a71e408397986ce7bfaf10
EightonLeePaulse12 <eightonleepaulse@gmail.com>
2023-08-21 14:09:49 +0200

    Error?

--- api/model/Users.js
+++ api/model/Users.js
@@ -1,6 +1,6 @@
 const db = require("../config");
 const { hash, compare, hashSync } = require("bcrypt");
-const {createToken} = require('../middleware/AuthenticateUser')
+const { createToken } = require("../middleware/AuthenticateUser");
 class Users {
   fetchUsers(req, res) {
     const query = `
@@ -146,4 +146,4 @@ class Users {
   }
 }
 
-module.exports = { Users }
+module.exports = { Users };
