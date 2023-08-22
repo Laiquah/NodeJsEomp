597b8837f5093dfdb2dbb502555cbdefa448f807
laiquah <cotielaiquah@gmail.com>
2023-08-22 20:00:33 +0200

    fixed a few minor things on about etc

--- frontend/src/components/UpdateUserComp.vue
+++ frontend/src/components/UpdateUserComp.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
         <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    update user
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal3">
+    edit
   </button>
   
   <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel3" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="exampleModalLabel3">Modal title</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -25,8 +25,8 @@
             <input type="text" placeholder="profile image">
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn">Save changes</button>
         </div>
       </div>
     </div>
@@ -41,5 +41,11 @@
 </script>
 
 <style scoped>
-
+.btn {
+  border: 2px solid #f7f4f1;
+  background-color: #f7f4f1;
+  margin-bottom: 1rem;
+  color: black;
+  box-shadow: 4px 4px black;
+}
 </style>
